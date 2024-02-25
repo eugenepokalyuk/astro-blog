0e6f21fa1be3f482287630cc88d17bc11bd7f5c3
Evgenii <eugene.pokalyuk@gmail.com>
2024-02-26 04:05:00 +0700

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config';
+
+import alpinejs from "@astrojs/alpinejs";
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap(), tailwind(), alpinejs()]
+});
---
+++ src/consts.ts
@@ -0,0 +1,2 @@
+export const SITE_TITLE = 'Nordic rose';
+export const SITE_DESCRIPTION = 'Welcome to Nordic blog';
